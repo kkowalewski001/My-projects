3e8b48997b2a9aa864959dc9a95f3d5f287bbe67
kkowalewski001 <126727689+kkowalewski001@users.noreply.github.com>
2023-03-10 13:29:03 +0100

    Add files via upload
    generating code for matlab, to extracting submatrix from the original one.

---
+++ matlab_matrix_generator.py
@@ -0,0 +1,106 @@
+import tkinter as tk
+
+def matGen(matrix):
+
+    b_x= 0
+    b_y= 0
+    wier = []
+    kolu = []
+
+    #długość kolumny
+    l = len(matrix)
+
+    #długośc rzędu
+    w = len(matrix[0])
+
+    #zakres wierszów
+    #help = [0,0,0]
+    help = [0] * w
+    for i in range(l):
+        for j in range(w):
+            if matrix[i][j] == 1:
+                help[j] = 1
+    for i in range(len(help)):
+        if help[i] == 1:
+            wier.append(i+1)
+    b_y = wier
+
+    #zakres kolumn
+    help = [0] * l
+    for i in range(w):
+        kolumna = i
+        kolumna = [element[kolumna] for element in matrix]
+        for j in range(len(kolumna)):
+                if kolumna[j] == 1:
+                    help[j] = 1
+
+    for i in range(len(help)):
+        if help[i] == 1:
+            kolu.append(i+1)
+    b_x = kolu
+
+
+    #pełny rząd check
+    for i in range(w):
+        if all(x == 1 for x in matrix[i]):
+            #print(f"pelny rząd nr {i+1}")
+            b_y = ":"
+            break
+
+    #pełna kolumna check
+    for i in range(w):
+        kolumna = i
+        kolumna = [element[kolumna] for element in matrix]
+        if all(x == 1 for x in kolumna):
+            #print(f"pelna kolumna nr {i+1}")
+            b_x = ":"
+            break
+
+    codeM = f"B = A({b_x},{b_y})"
+    if codeM == "B = A(:,:)":
+        return "B = A"
+    else:
+        return str(codeM)
+# Function to toggle a button's state and update the matrix
+def toggle(button, matrix, row, col):
+    if button['bg'] == 'red':
+        button.config(text = "B",bg='green')
+        matrix[row][col] = 1
+    else:
+        button.config(text = "A",bg='red')
+        matrix[row][col] = 0
+    matrix_label.config(text=matGen(matrix))
+
+# Get user input for the number of rows and columns
+num_rows = int(input("Enter the number of rows: "))
+num_cols = int(input("Enter the number of columns: "))
+
+# Create a nested list in memory initialized to 0
+matrix = [[0 for j in range(num_cols)] for i in range(num_rows)]
+
+#m = matGen(matrix)
+
+# Create a new window with increased height and width
+window = tk.Tk()
+window.geometry("500x250")
+
+# Create a matrix of buttons initialized to 0
+buttons = []
+for i in range(num_rows):
+    row = []
+    for j in range(num_cols):
+        button = tk.Button(window, text='A', bg='red', width=5, height=2, command=lambda x=i, y=j: toggle(buttons[x][y], matrix, x, y))
+        button.grid(row=i, column=j)
+        row.append(button)
+    buttons.append(row)
+
+# Add a label widget on the far right side of the window to display the nested list
+matrix_label = tk.Label(window, text=matGen(matrix), font=("Arial Bold", 12))
+matrix_label.grid(row=0, column=num_cols+1, rowspan=num_rows+1, sticky="e")
+
+# Add a label widget on the far right side of the window to display the label
+label = tk.Label(window, text="Kod do Matlaba", font=("Arial Bold", 12))
+label.grid(row=0, column=num_cols+2, sticky="e")
+
+# Start the GUI event loop
+window.mainloop()
